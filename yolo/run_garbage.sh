a375d05dc891f111a62efb352deb50ebc8b56423
yjh0410 <1394571815@qq.com>
2024-04-09 10:32:51 +0800

    add garbage

--- yolo/run_garbage.sh
+++ yolo/run_garbage.sh
@@ -1,4 +1,4 @@
 
 
 # -------------------------- Train Pipeline --------------------------
-python -m torch.distributed.run --nproc_per_node=${1} --master_port 9000 garbage.py --cuda
+python -m torch.distributed.run --nproc_per_node=1 --master_port 9000 garbage.py --cuda
