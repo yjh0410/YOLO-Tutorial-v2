e84da168959b9d66d782decef18f2a019315e665
yjh0410 <1394571815@qq.com>
2024-04-11 08:08:38 +0800

    update

--- odlab/config/__init__.py
+++ odlab/config/__init__.py
@@ -6,13 +6,17 @@ from .detr_config      import build_detr_config
 def build_config(args):
     # FCOS
     if "fcos" in args.model:
-        return build_fcos_config(args)
+        cfg = build_fcos_config(args)
     # YOLOF
     elif "yolof" in args.model:
-        return build_yolof_config(args)
+        cfg = build_yolof_config(args)
     # DETR
     elif "detr" in args.model:
-        return build_detr_config(args)
-    
+        cfg = build_detr_config(args)
     else:
         raise NotImplementedError('Unknown Model: {}'.format(args.model))
+
+    # Print model config
+    cfg.print_config()
+
+    return cfg
