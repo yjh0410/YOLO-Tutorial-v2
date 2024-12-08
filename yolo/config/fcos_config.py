822b71c138d948b8d490d1cf2660bbb1f7b22d7c
yjh0410 <1394571815@qq.com>
2024-12-08 13:47:15 +0800

    update

--- yolo/config/fcos_config.py
+++ yolo/config/fcos_config.py
@@ -50,9 +50,9 @@ class FcosBaseConfig(object):
         # ---------------- Optimizer config ----------------
         self.trainer      = 'yolo'
         self.optimizer    = 'adamw'
-        self.base_lr      = 0.001     # base_lr = per_image_lr * batch_size
+        self.base_lr      = 0.0001     # base_lr = per_image_lr * batch_size
         self.min_lr_ratio = 0.01      # min_lr  = base_lr * min_lr_ratio
-        self.batch_size_base = 64
+        self.batch_size_base = 16
         self.momentum     = 0.9
         self.weight_decay = 0.05
         self.clip_max_norm   = 35.0
