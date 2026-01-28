aee68d102cffd40831dd6a384f57b0d5e5938ad0
yjh0410 <1394571815@qq.com>
2026-01-28 23:22:46 +0800

    update demo

--- yolo/demo.py
+++ yolo/demo.py
@@ -20,7 +20,6 @@ from config import build_config
 
 from dataset.voc  import voc_class_labels
 from dataset.coco import coco_class_labels
-from yolo.dataset.custom import custom_class_labels
 
 
 def parse_args():
@@ -57,7 +56,7 @@ def parse_args():
 
     return parser.parse_args()
                     
-
+@torch.no_grad()
 def detect(args,
            model, 
            device, 
@@ -249,22 +248,9 @@ def detect(args,
                 cv2.imshow('detection', img_processed)
                 cv2.waitKey(0)
 
-
 def run():
     args = parse_args()
-    # Dataset config
-    if   args.dataset == "voc":
-        cfg.num_classes = 20
-        cfg.class_labels = voc_class_labels
-    elif args.dataset == "coco":
-        cfg.num_classes = 80
-        cfg.class_labels = coco_class_labels
-    elif args.dataset == "custom":
-        cfg.num_classes = len(custom_class_labels)
-        cfg.class_labels = custom_class_labels
-    else:
-        raise NotImplementedError("Unknown dataset: {}".format(args.dataset))
-    
+
     # cuda
     if args.cuda and torch.cuda.is_available():
         print('use cuda')
@@ -275,6 +261,16 @@ def run():
     # Build config
     cfg = build_config(args)
 
+    # Dataset config
+    if   args.dataset == "voc":
+        cfg.num_classes = 20
+        cfg.class_labels = voc_class_labels
+    elif args.dataset == "coco":
+        cfg.num_classes = 80
+        cfg.class_labels = coco_class_labels
+    else:
+        raise NotImplementedError("Unknown dataset: {}".format(args.dataset))
+    
     # Build model
     model = build_model(args, cfg, False)
 
